052809f8255a8162612d9085eaa626ac28f44410
akshar-bhalani <akshar.bhalani@bacancy.com>
2025-06-25 16:00:42 +0530

    render data from the list and importance of the keys

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button.jsx";
-
+import Text from "./Text.jsx";
 const App = () => { 
 
-    const handleClickAction = () => {
-        console.log("i was clicked from perent component");
+    const [data,setData] = useState([
+        {id:'a',text:"text 1"},
+        {id:'b',text:"text 2"},
+        {id:'c',text:"text 3"},
+        {id:'d',text:"text 4"},
+        {id:'e',text:"text 5"},
+    ]);
+
+  
+    const addMoreData = () => {
+        setData((prevData) =>{
+          return  [{id:'f',text:"text 6"},...prevData]
+        })
     };  
 
+    
 
-
-    return <Button data={{a:{s:{d:'c'}}}} clickAction={handleClickAction}>click me</Button>;
+    return <>
+        {
+            data.map(((item,index)=><Text key={item.id}>{item.text}</Text>))
+        }
+        <Button clickAction={addMoreData} >Add more data</Button>
+    </>
 }
-
 export default App;
