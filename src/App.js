c6ef8738772dd65ada8ebe65469e50fb9d91731d
akshar1277 <bhalani.akshar1277@gmail.com>
2025-06-26 16:34:11 +0530

    create portal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState,useEffect,useRef,lazy,Suspense} from "react";
+
 import Button from "./Button.jsx";
 // import Text from "./Text.jsx";
 import Timer from "./Timer.jsx";
@@ -10,15 +11,18 @@ import PrintTable from "./PrintTable.jsx";
 import Heading from "./Heading.jsx";
 import Navbar from "./Navbar.jsx";
 import { ThemeContext } from "./context.jsx";
+import { createPortal } from "react-dom";
 
 const Text = lazy(()=>import('./Text.jsx'))
 
 const App = () => { 
-    const [theme,setTheme] = useState('dark');
-   return<ThemeContext.Provider value={[theme,setTheme]}>
-    <Navbar/>
-    
-
-   </ThemeContext.Provider> 
+    const [showModel,toggleModel] = useState(false);
+   return<>
+   Hey i am inside root 
+   <button onClick={()=>toggleModel((prev)=>!prev)}>Toggle Modal </button>
+   {
+    showModel && createPortal(<div>This is model contnet</div>,document.body)
+   }
+   </>
 }
 export default App;
